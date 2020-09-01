fdb34c4aa960379f8059156dc8e57623490ca2e8
Niklas <n.hakansson@hotmail.com>
2020-09-01 18:17:38 +0200

    Deletet the default vue shit and just added a home page in order to see of the class based syntax works

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <router-view />
+  <div class="HomePage">
+    <h1>HomePage</h1>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 import { Vue, Component } from "vue-property-decorator";
 
 @Component({})
-export default class App extends Vue {}
+export default class HomePage extends Vue {}
 </script>
 
 <style></style>
