e50a851f208c24e66b943535665ff7b1ca4ea915
Niklas <n.hakansson@hotmail.com>
2020-09-15 15:17:19 +0200

    started search query

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -3,11 +3,17 @@ const path = require("path");
 const dbPath = path.join(__dirname, "../databases/foodStore.db");
 const db = new DB(dbPath);
 
-// const getSomething = async (req, res) => {
-//   await db.all(/*sql*/ `query....osv...`);
-//   //Code here
-// };
+const getProducts = async (req, res) => {
+  let s = req.query.s ? req.query.s.split(",") : null;
+  let c = req.query.c ? req.query.c.split(",") : null;
+  let d = req.query.d ? req.query.d.split(",") : null;
+
+  // let dietaryResults = await db.all(/*sql*/ `SELECT * FROM Dietary_Restrictions WHERE `)
+
+  console.log(s, c, d);
+  res.json(null); 
+};
 
 module.exports = {
-  // getSomething,
+  getProducts,
 };
--- backend/routes/apiRoutes.js
+++ backend/routes/apiRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const apiController = require("../controllers/apiController");
 
 // router.get("/something", apiController.getSomething);
+router.get("/products", apiController.getProducts)
 
 module.exports = router;
