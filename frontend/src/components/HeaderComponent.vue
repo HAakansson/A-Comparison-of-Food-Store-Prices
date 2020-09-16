5a92a783f509087bda24644d267f92227bdec3d4
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-16 14:37:56 +0200

    Added Header Component

---
+++ frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header-component">
+    <h1>The Price Comparitor</h1>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Watch } from "vue-property-decorator";
+// Vue and Component must always be imported from the vue-property-decorater. If you are going to need Props and Watches, it must be imported from the vue-property-decorator.
+
+// @Component must always be written directly above export default.
+@Component({
+  // In this components object, you must add the nested components that you use.
+  components: {
+    //Some Component.
+  },
+})
+export default class HeaderComponent extends Vue {
+
+  searchedString = "";
+
+
+
+  @Watch("searchedString", { deep: true })
+  onSomeData(oldVal, newVal) {
+    // The method to be run when the variable changes must be declared directly underneath the @Watch.
+    console.log(oldVal, newVal);
+    this.searchedString = newVal;
+  }
+
+
+
+  created() {
+    console.log("CREATED");
+  }
+  mounted() {}
+  beforeDestroy() {}
+}
+</script>
+
+<style lang="scss" scoped></style>
+
--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home-page">
+    <HeaderComponent />
     <h1>HomePage</h1>
     <div class="test">
       <h1>Sass Test</h1>
@@ -9,8 +10,14 @@
 
 <script>
 import { Vue, Component } from "vue-property-decorator";
+import HeaderComponent from "../components/HeaderComponent.vue";
 
-@Component({})
+
+@Component({
+  components: {
+    HeaderComponent
+  }
+})
 export default class HomePage extends Vue {}
 </script>
 
