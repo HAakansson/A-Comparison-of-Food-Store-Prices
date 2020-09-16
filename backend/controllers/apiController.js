145921657a9701b9af34dfc61793f04e65a13589
Niklas <n.hakansson@hotmail.com>
2020-09-16 22:24:07 +0200

    done with seachRoute, testes as much as I can

--- backend/controllers/apiController.js
+++ backend/controllers/apiController.js
@@ -4,14 +4,63 @@ const dbPath = path.join(__dirname, "../databases/foodStore.db");
 const db = new DB(dbPath);
 
 const getProducts = async (req, res) => {
-  let s = req.query.s ? req.query.s.split(",") : null;
-  let c = req.query.c ? req.query.c.split(",") : null;
-  let d = req.query.d ? req.query.d.split(",") : null;
+  let searchArr = req.query.s ? req.query.s.split(" ") : null;
+  let dietArr = req.query.d ? req.query.d.split(",") : null;
+  let catArr = req.query.c ? req.query.c.split(",") : null;
+  console.log(catArr);
 
-  // let dietaryResults = await db.all(/*sql*/ `SELECT * FROM Dietary_Restrictions WHERE `)
+  let searchString = "";
+  if (searchArr) {
+    searchArr.forEach((s, i) => {
+      searchString +=
+        i === 0 ? `WHERE p.name LIKE "%${s}%" ` : `AND p.name LIKE "%${s}%" `;
+    });
+  }
 
-  console.log(s, c, d);
-  res.json(null); 
+  let dietString = "";
+  if (dietArr) {
+    dietArr.forEach((d) => {
+      dietString += `AND dr.${d} = true `;
+    });
+  }
+
+  let catString = catArr ? "AND c.name IN (" : "";
+  let catChecks = "";
+  if (catArr) {
+    catArr.forEach((c, i) => {
+      catChecks +=
+        i === catArr.length - 1
+          ? `"${c[0].toUpperCase() + c.slice(1)}"`
+          : `"${c[0].toUpperCase() + c.slice(1)}",`;
+    });
+    catString += `${catChecks})`;
+  }
+
+  let query = /*sql*/ `SELECT p.id, p.name, p.store, p.unit_price, p.comparison_price, p.comparator, p.display_volume, p.unit_measurement, p.country_of_origin, dr.*, i.*,
+    CAST(LENGTH("${searchArr.join(
+      " "
+    )}") AS FLOAT) / LENGTH(p.name) as matchedSearchString
+    FROM Product as p
+    JOIN Dietary_Restrictions as dr ON p.dietary_restrictions_id = dr.id
+    JOIN Image as i ON p.image_id = i.id
+    JOIN ProductsXCategories as pxc ON p.id = pxc.productId
+    JOIN Category as c ON c.id = pxc.categoryId
+    ${searchString}
+    ${dietString}
+    ${catString}
+    ORDER BY matchedSearchString DESC`;
+
+  let results = await db.all(query);
+
+  let hash = {};
+  results.forEach((r) => {
+    hash[r.id] = r;
+  });
+  results = Object.values(hash);
+
+  results.sort((a, b) => b.matchedSearchString - a.matchedSearchString);
+
+  res.json(results);
 };
 
 module.exports = {
