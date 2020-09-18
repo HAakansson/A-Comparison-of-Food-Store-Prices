2d9c75e8e22aef8853ed48ac0bdacf586311185c
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-09-18 09:10:37 +0200

    Started working on sql for fetching the cheapest product

--- backend/shopping_list_calculator/CalculateShoppingList.js
+++ backend/shopping_list_calculator/CalculateShoppingList.js
@@ -5,4 +5,14 @@ module.exports = class CalculateShoppingList {
     console.log("Shopping List to calculate ", shoppingList);
   }
 
+  static  async fetchCheapestProducts(shoppingList){
+    //fetch products from the shoppinglist, based on name? and fetch the cheapest products.
+    for (let product of shoppingList){
+      await db.run(/*sql*/ `SELECT * FROM Product WHERE name LIKE $%productName%`,
+      {
+        $name: product.name
+      })
+    }
+  }
+
 }
