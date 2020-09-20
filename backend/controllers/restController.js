664cd4558efc8b9229cb8e0ccd207ce4da517b66
Niklas <n.hakansson@hotmail.com>
2020-09-20 19:10:13 +0200

    nothing

--- backend/controllers/restController.js
+++ backend/controllers/restController.js
@@ -2,8 +2,17 @@ const DB = require("../DB");
 const path = require("path");
 const dbPath = path.join(__dirname, "../databases/foodStore.db");
 const db = new DB(dbPath);
-const CalculateShoppingList = require('../shopping_list_calculator/CalculateShoppingList');
+const CalculateShoppingList = require("../shopping_list_calculator/CalculateShoppingList");
 
+const getDietaryRestrictions = async (req, res) => {
+  let result = await db.all(/*sql*/ `SELECT * FROM Dietary_Restrictions`);
+  res.json(result);
+};
+
+const postShoppingList = async (req, res) => {
+  //var jsonList = JSON.stringify(req.body);
+  CalculateShoppingList.calculateTotalCost(req.body);
+};
 const getCategories = async (req, res) => {
   let results = await db.all(/*sql*/ `SELECT DISTINCT name, 
     CAST(LENGTH("${req.query.search}") AS FLOAT)/LENGTH(name) as match_percentage,
@@ -15,18 +24,8 @@ const getCategories = async (req, res) => {
   res.json(results);
 };
 
-const getDietaryRestrictions = async (req, res) => {
-  let result = await db.all(/*sql*/ `SELECT * FROM Dietary_Restrictions`);
-};
-
-
-const postShoppingList = async (req, res) => {
-
-  //var jsonList = JSON.stringify(req.body);
-  CalculateShoppingList.calculateTotalCost(req.body);
-}
-
 module.exports = {
   getCategories,
   getDietaryRestrictions,
+  postShoppingList
 };
