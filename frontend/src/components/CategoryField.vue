cd047ffc7aea2c6570f6d3c9f2009ec127d22aed
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 13:45:57 +0200

    Added category component

---
+++ frontend/src/components/CategoryField.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="category-field">
+      <input type="text" placeholder="Search for category..."/>
+  </div>
+</template>
+
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class CategoryField extends Vue {
+
+// search = "";
+
+
+// @Watch("search")
+//   onSearchStringChanged(newVal) {
+
+//     this.$store.state.searchQueries.searchString = "?s=";
+//     this.$store.state.searchQueries.searchString += newVal;
+//     console.log(this.$store.state.searchQueries);
+//   }
+// }
+
+
+
+
+}
+
+</script>
+
+<style scoped>
+
+input {
+  width: 100%;
+  height: 30px;
+  font-size: 14px;
+  
+}
+
+</style>
