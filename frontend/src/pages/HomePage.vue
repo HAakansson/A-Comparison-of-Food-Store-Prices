64cda0b40669d0e36608f07477fdac25815cb5e2
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 11:36:10 +0200

    Cleaned up Homepage

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,23 +1,13 @@
 <template>
   <div class="home-page">
-    <HeaderComponent />
-    <h1>HomePage</h1>
-    <div class="test">
-      <h1>Sass Test</h1>
-    </div>
   </div>
 </template>
 
 <script>
 import { Vue, Component } from "vue-property-decorator";
-import HeaderComponent from "../components/HeaderComponent.vue";
 
 
-@Component({
-  components: {
-    HeaderComponent
-  }
-})
+@Component
 export default class HomePage extends Vue {}
 </script>
 
