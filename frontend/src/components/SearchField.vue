06de49faa03bcce4f1ac6565ea988e7ba9665101
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 13:46:15 +0200

    added search field component for search field

---
+++ frontend/src/components/SearchField.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="search-field">
+      <input v-model="search" type="text" placeholder="Search for product..."/>
+  </div>
+</template>
+
+
+<script>
+import { Vue, Component, Watch } from "vue-property-decorator";
+
+
+@Component
+export default class SearchField extends Vue {
+
+search = "";
+
+
+@Watch("search")
+  onSearchStringChanged(newVal) {
+
+    this.$store.state.searchQueries.searchString = "?s=";
+    this.$store.state.searchQueries.searchString += newVal;
+    // console.log(this.$store.state.searchQueries);
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+input {
+  width: 100%;
+  height: 30px;
+  font-size: 14px;
+  
+}
+
+</style>
+
