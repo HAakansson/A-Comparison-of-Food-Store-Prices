517117b805c1be08d214692eced79b629b042816
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 11:36:23 +0200

    Added component for checkbox

---
+++ frontend/src/components/Checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="diet-checkbox">
+    <label :for="`diet-checkbox-${name}`">{{name}}</label>
+    <input :id="`diet-checkbox-${name}`" type="checkbox">
+  </div>
+</template>
+
+
+<script>
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Checkbox extends Vue {
+
+
+
+@Prop({
+  type: String,
+  required: true,
+})
+name;
+
+}
+
+</script>
+
+<style scoped>
+
+.diet-checkbox {
+  display: flex;
+  margin: 0 5px;
+}
+</style>
+
