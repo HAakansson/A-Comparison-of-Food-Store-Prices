15db0014c90dd1d7125eb21ea831885047ba5852
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 11:36:37 +0200

    Fixed styling for header component

--- frontend/src/components/HeaderComponent.vue
+++ frontend/src/components/HeaderComponent.vue
@@ -1,36 +1,65 @@
 <template>
   <div class="header-component">
-    <header>
-       <h1>Prisjämföraren</h1>
-       <form>
-         <!-- <input v-bind:value="searchedString" class="search-field" type="text" placeholder="Search for product..."/> -->
-         <input v-model="searchedString" class="search-field" type="text" placeholder="Search for product..."/>
-          <button v-on:click="searchButtonPressed" type="button">Search</button>
-       </form>
+      <h1>Prisjämföraren</h1>
+      <div class="grind-container">
+        <div class="grid-item-1">
+          <div class="i1">
+            <!-- <h2>product</h2> -->
+                <input v-model="searchedProduct" class="search-field" type="text" placeholder="Search for product..."/>
+          </div>
+        </div>
+        <div class="grid-item-2">
+          <div class="i2">
+            <!-- <h2>category</h2> -->
+                <input v-model="searchedProduct" class="search-field" type="text" placeholder="Search for Category..."/>
+          </div>
+        </div>
+        <div class="grid-item-3">
+          <div class="diet-checkboxes" v-for="(diet, i) in dietaryRestrictions" :key="i">
+              
+            <Checkbox :name="diet.name"/>
 
-       <!-- <img src="https://banner2.cleanpng.com/20171217/01f/shopping-cart-png-5a364b6d3217e8.4884266315135076932052.jpg" /> -->
-    </header>
+          </div>
+        </div>
+     
+      </div>
   </div>
 </template>
 
 <script>
 import { Vue, Component, Watch } from "vue-property-decorator";
-// Vue and Component must always be imported from the vue-property-decorater. If you are going to need Props and Watches, it must be imported from the vue-property-decorator.
+import Checkbox from "./Checkbox";
 
-// @Component must always be written directly above export default.
 @Component({
-  // In this components object, you must add the nested components that you use.
   components: {
-    //Some Component.
+    Checkbox
   },
 })
 export default class HeaderComponent extends Vue {
 
-  searchedString = "";
+  searchedProduct = "";
 
+  dietaryRestrictions = [
+    {
+      name: "Vegan"
+    }, 
+    {
+      name: "Vegetarian"
+    }, 
+    {
+      name: "Gluten"
+    }, 
+    {
+      name: "Organic"
+    }, 
+    {
+      name: "Lactosfree"
+    }, 
 
+    
+  ]
 
-  @Watch("searchedString", { deep: true })
+  @Watch("searchedProduct", { deep: true })
   onSomeData(product) {
     // The method to be run when the variable changes must be declared directly underneath the @Watch.
     
@@ -40,8 +69,13 @@ export default class HeaderComponent extends Vue {
 
 
   searchButtonPressed() {
+    
     console.log("Searching...");
-    console.log(this.searchedString);
+    console.log(this.searchedProduct);
+
+    //this.$router.push({ path: '/' });
+
+
   }
 
   searchForProduct(product) {
@@ -57,27 +91,88 @@ export default class HeaderComponent extends Vue {
 
 <style lang="scss" scoped>
 .header-component{
-  margin-top: 0px;
-  background-color: rgb(195, 226, 147);
-}
-
-.search-field{
-  background: #fff;
-  color: #666;
-  font: 90%/180% Arial, Helvetica, sans-serif;
-  width: 800px;
-  max-width: 96%;
-  margin: 0 auto;
+  background-image: url("https://www.ruohonjuuri.fi/media/wysiwyg/Happy_Food_Store.JPG");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 30vh;
+  color:white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-bottom: 5px;
+  text-shadow:  0px -1px black, 1px 0px black, 0px 1px black, -1px 0px black;
+  
 }
 
 h1 {
-  margin-top: 0px;
-  padding-top: 40px;
-  padding-bottom: 15px;
-  font-size: 45px;
+  margin-top: 0;
+  padding-top: 50px;
   text-align: center;
-  color: rgb(75, 104, 104);
-  text-decoration: underline;
+  font-size: 3.2em;
+
 }
+
+.i1 {
+  background-color: lightcoral;
+}
+.i2 {
+  background-color: rebeccapurple;
+}
+
+input {
+  width: 100%;
+  height: 30px;
+  font-size: 14px;
+  // padding: 0;
+  // margin: 0;
+}
+
+
+
+.grind-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 5px;
+  padding: 0px 5px;
+  // grid-template-areas: 
+  //   "searchinput searchinput categoryinput categoryinput"
+  //   ". dietaryinput dietaryinput ."
+  
+}
+
+
+.grid-item-1 {
+  grid-column: 1/3;
+  grid-row: 1/2;
+  // grid-area: searchinput;
+  // margin: auto;
+  //overflow: hidden;
+
+}
+
+
+.grid-item-2 {
+  grid-column: 3/5;
+  grid-row: 1/2;
+  // grid-area: categoryinput;
+  // margin: auto;
+
+  //overflow: hidden;
+
+}
+
+
+
+
+.grid-item-3 {
+  grid-column: 1/5;
+  grid-row: 2/3;
+  display: flex;
+  justify-content: center;
+
+  // grid-area: dietaryinput;
+}
+
 </style>
 
