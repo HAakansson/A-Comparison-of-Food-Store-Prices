25b446b19abb21d0343ad0dd26ef3314a5f8b19b
Henrik Jangefelt <henke9105@hotmail.com>
2020-09-22 15:47:15 +0200

    Added articleList component to HomePage

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home-page">
+    <h1>Articles</h1>
+    <div>
+      <ArticleList />
+    </div>
   </div>
 </template>
 
 <script>
 import { Vue, Component } from "vue-property-decorator";
+import ArticleList from "../components/ArticleList";
+
+@Component({
+  components: {
+      ArticleList,
+  }
+})
+export default class HomePage extends Vue {
 
 
-@Component
-export default class HomePage extends Vue {}
+
+}
+
+
+
 </script>
 
 <style lang="scss" scoped>
